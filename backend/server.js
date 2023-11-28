79344d9ea925daab3acfd382ca8d3a50a3bc1517
Fred Bolder <fgh.bolder@gmail.com>
2023-11-28 19:31:07 +0100

    front in back 5

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../frontend/dist/')))
+
+app.use(express.static(path.resolve("./frontend/dist/")))
 
 app.use(cookieParser())
 
