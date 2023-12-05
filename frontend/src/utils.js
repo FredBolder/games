45c16628d3aa39d63b67208fd0b64f8fe525d37f
Phoenix <Phoenix@example.com>
2023-12-05 13:51:25 +0100

    validation

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,11 +1,40 @@
-
+function capitalize(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+}
 
 export function fixUserData(user) {
-
+  user.firstName = user.firstName.trim();
+  user.firstName = capitalize(user.firstName);
+  user.userName = user.userName.trim();
+  user.userName = capitalize(user.userName);
+  user.email = user.email.trim().toLowerCase();
+  user.password = user.password.trim();
+  user.password2 = user.password2.trim();
 }
 
 export function validateUserData(user) {
-    let msg = "";
-    msg = "THIS IS A TEST ERROR!!"
-    return msg;
+  let msg = "";
+
+  function addEnter() {
+    if (msg !== "") {
+      msg += "\n";
+    }
+  }
+
+  if (user.firstName.length < 2) {
+    msg += "First Name is too short!";
+  }
+  if (user.userName.length < 2) {
+    addEnter();
+    msg += "User Name is too short!";
+  }
+  if (user.password.length < 4) {
+    addEnter();
+    msg += "Password is too short!";
+  }
+  if (user.password !== user.password2) {
+    addEnter();
+    msg += "Confirm Password does not match!";
+  }
+  return msg;
 }
