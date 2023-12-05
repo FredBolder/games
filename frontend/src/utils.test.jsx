fd888b35b62e40a025bd3e3e409d759974163c30
Fred Bolder <fgh.bolder@gmail.com>
2023-12-05 09:47:33 +0100

    preparation validation registration

---
+++ frontend/src/utils.test.jsx
@@ -0,0 +1,23 @@
+import { describe, it, expect } from "vitest";
+import { fixUserData, validateUserData } from "./utils";
+
+const input1 = {
+  firstName: "jOhN",
+  userName: " herO",
+  email: "John@mail.com ",
+  password: " 123",
+  password2: "123",
+};
+
+const expectedOutput1 = {
+  firstName: "John",
+  userName: "Hero",
+  email: "john@mail.com",
+  password: "123",
+  password2: "123",
+};
+
+fixUserData(input1);
+it("fixUserData", () => {
+  expect(JSON.stringify(input1)).toBe(JSON.stringify(expectedOutput1));
+});
