4b2220693805827e5c571758f099bfcfea071bf0
mkotkowicz <michal.kotkowicz@gmail.com>
2023-12-06 13:43:45 +0100

    michal login styling

--- frontend/src/Components/LoginPage.jsx
+++ frontend/src/Components/LoginPage.jsx
@@ -6,6 +6,7 @@ import InfoContext from "../Context/InfoContext";
 import Navbar from "./Navbar";
 import axios from "axios";
 import Footer from "./Footer";
+import Tennis from "../Images/tennis.svg";
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -37,30 +38,43 @@ function LoginPage() {
           <Navbar />
         </header>
         <div className="loginBox">
-          <div className="loginBoxInfo">
+          <div className="loginBoxInfo loginBoxOne">
+            <div>
+              <h3>Login:</h3>
+            </div>
+            <div>
+              <form onSubmit={submitHandler}>
+                <p className="form-label">Email:</p>
+                <input name="email" type="email" />
+                <p className="form-label">Password:</p>
+                <input name="password" type="password" />
+                <br />
+                <div className="loginSubmit">
+                  <input type="submit" value="Login" className="button" />
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="loginBoxInfo loginBoxTwo">
             <div className="loginBoxInfos">
               <div>
-                <h3>Login Here:</h3>
-              </div>
-              <div>
-                <form onSubmit={submitHandler}>
-                  <p>Email:</p>
-                  <input name="email" type="email" />
-                  <p>Password:</p>
-                  <input name="password" type="password" />
-                  <br />
-                  <input type="submit" value="Login" className="button" />
-                </form>
+                <h3>If you haven't signed up with us yet:</h3>
+                <p>
+                  Unlock the full potential of our site by creating an account.
+                  It's a breeze and takes just a couple of minutes.
+                </p>
+                <p>Ready to get started? Click the button below to register:</p>
+                <div className="loginToRegister">
+                  <Link className="button loginLink" to="/register">
+                    Register
+                  </Link>
+                </div>
               </div>
             </div>
             <div className="loginBoxInfos">
-              <h3>If you don't have an account yet:</h3>
-              <p>
-                To fully use our site, you need to make an account, what only
-                takes 2 minutes.
-              </p>
-              <p>To make an account, click here:</p>
-              <Link to="/register">Register</Link>
+              <div className="loginPicture">
+                <img src={Tennis} alt="Tennis" />
+              </div>
             </div>
           </div>
         </div>
