d88172e40659dd719b75df8923f55f0085e5652e
Fred Bolder <fgh.bolder@gmail.com>
2024-02-18 13:00:29 +0100

    style improved

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 import { useContext } from "react";
 import InfoContext from "../Context/InfoContext";
