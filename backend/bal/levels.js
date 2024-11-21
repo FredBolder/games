9d6541adb5f1cfc1f5cb015f227c2cef53e98e68
Fred Bolder <bolder@muenzgalerie.de>
2024-11-21 11:28:46 +0100

    test 10

--- backend/bal/levels.js
+++ backend/bal/levels.js
@@ -55,7 +55,7 @@ function loadFromFile(n) {
 function getLevel(n) {
   let data = [];
 
-  if ((n >= 200 && n <= 219) || (n >= 700 && n <= 715) || (n >= 750 && n <= 760)) {
+  if ((n >= 200 && n <= 219) || (n >= 700 && n <= 715) || (n >= 750 && n <= 760) || (n >= 990 && n <= 990)) {
     data = loadFromFile(n);
   } else {
     data = loadFromFile(1000);
